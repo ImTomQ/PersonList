2ff295022246df60d44a175935a1bd8cd8ef08a5
Nguyen Quyen <nguyenquyen@example.com>
2020-07-22 23:02:46 +0700

    Updates

---
+++ precache-manifest.f2f6a8fbbaeb8d5e082b280a8dc7a183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7b507021422916e310e476c55a51a0",
+    "url": "/PersonList/index.html"
+  },
+  {
+    "revision": "381974346910c79af222",
+    "url": "/PersonList/static/css/main.3b4500f3.chunk.css"
+  },
+  {
+    "revision": "3c5b686fbcf4fe291fdb",
+    "url": "/PersonList/static/js/2.ccd7c0a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PersonList/static/js/2.ccd7c0a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "381974346910c79af222",
+    "url": "/PersonList/static/js/main.a07a98f5.chunk.js"
+  },
+  {
+    "revision": "94eea37983e1646488b1",
+    "url": "/PersonList/static/js/runtime-main.90048f62.js"
+  }
+]);
